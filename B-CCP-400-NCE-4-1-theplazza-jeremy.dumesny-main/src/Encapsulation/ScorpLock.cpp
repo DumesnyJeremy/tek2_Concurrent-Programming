0702d84ac10e8e0fda5bd487438dfa33e5d44282
DumesnyJeremy <60926613+DumesnyJeremy@users.noreply.github.com>
2023-04-04 19:02:42 -0400

    Delete B-CCP-400-NCE-4-1-theplazza-jeremy.dumesny-main directory

--- B-CCP-400-NCE-4-1-theplazza-jeremy.dumesny-main/src/Encapsulation/ScorpLock.cpp
+++
@@ -1,8 +0,0 @@
-/*
-** EPITECH PROJECT, 2021
-** plazza [WSL: Ubuntu]
-** File description:
-** ScorpLock
-*/
-
-#include "ScorpLock.hpp"
--- B-CCP-400-NCE-4-1-theplazza-jeremy.dumesny-main/src/Encapsulation/Thread.cpp
+++
@@ -1,8 +0,0 @@
-/*
-** EPITECH PROJECT, 2021
-** plazza [WSL: Ubuntu]
-** File description:
-** Thread
-*/
-
-#include "Thread.hpp"
